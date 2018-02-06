8e661fb6c37f294330f085553c9fb63473523b5c
THANVEER <thanveersiddiq@gmail.com>
2018-02-06 23:28:40 +0530

    group task codes added

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -7,7 +7,7 @@ from wtforms import Form, validators, StringField
 filestore_url = "https://filestore.helmet94.hasura-app.io/v1/file"
 data_url = "https://data.helmet94.hasura-app.io/v1/query"
 filestore_headers = {
-   "Authorization": "Bearer d3df3ca86db4b0f131ae030cf4dceb053f82c8cb624cd01b"
+   "Authorization": "Bearer 5a021247deb17e3b4e93a4d99f5b0cf09ac0c880d777bec0"
 }
 data_headers = {
    "Content-Type": "application/json"
@@ -98,7 +98,7 @@ def upload():
             print("not exist")
             return "failed"
     else:
-        return render_template('Image_upload_form.html')
+        return render_template('image_upload_form.html')
 
 @app.route('/uploadPost', methods=['POST'])
 def upload_post():
@@ -107,16 +107,104 @@ def upload_post():
     file_key = request_json['file_key']
     user_id = request_json['user_id']
     descr = request_json['descr']
+    tags_string=request_json['tags']
     query= {
-   "type": "insert",
-   "args":{
-        "table":"user_post",
-        "objects":[
-            {"image_url": file_key ,"created_by": user_id ,"description": descr}
-        ],
-        "returning":["post_id"]
+        "type": "insert",
+        "args":{
+            "table":"user_post",
+            "objects":[
+                {"image_url": file_key ,"created_by": user_id ,"description": descr}
+            ],
+            "returning":["post_id"]
+        }
     }
-   }
     resp = requests.request("POST", data_url, data=json.dumps(query), headers=data_headers)
+    response = resp.content
+    jsonResponse = json.loads(response.decode('utf-8'))
+    post_id= jsonResponse['returning'][0]['post_id']
+    tag_added=add_tags(tags_string,post_id)
+    if(tag_added):
+        return "success"
+    else:
+        return "upload failed"
 
-    return "success"	
+@app.route('/getPosts', methods=['GET'])
+def get_post():
+    query = {
+        "type":"select",
+        "args": {
+            "table": "user_post",
+            "columns": ["*"],
+            "where": {"post_id": {"$lt":"40"}},
+            "limit":"5",
+            "order_by": "-post_id",
+        }
+    }
+    resp = requests.request("POST", data_url, data=json.dumps(query), headers=data_headers)
+    json_res = decode_json(resp)
+    print(json_res)
+    return jsonify(posts=json_res)
+
+
+def add_tags(tag_string,post_id):
+    post_tag_ids=[]
+    tags = tag_string.split('|')
+    for tag in tags:
+        print(tag)
+        requestPayload = {
+            "type": "select",
+            "args": {
+                "table": "user_tags",
+                "columns": ["tag_id"],
+                "where": {
+                    "tag_name": tag
+                }
+            }
+        }
+        resp = requests.request("POST", data_url, data=json.dumps(requestPayload), headers=data_headers)
+        json_res = decode_json(resp)
+        if(len(json_res) > 0):
+            tag_id = json_res[0]['tag_id']
+        else:
+            requestPayload1 = {
+                "type": "insert",
+                "args": {
+                    "table": "user_tags",
+                    "objects": [
+                        {
+                            "tag_name": tag,
+                            "created_by": 1
+                        }
+                    ],
+                    "returning": [
+                        "tag_id"
+                    ]
+                }
+            }
+            resp1 = requests.request("POST", data_url, data=json.dumps(requestPayload1), headers=data_headers)
+            tag_id= decode_json(resp1)['returning'][0]['tag_id']
+            print('tag_id:'+str(tag_id))
+
+        requestPayload2= {
+            "type": "insert",
+            "args":{
+                "table":"post_tags",
+                "objects":[
+                    {"post_id":post_id ,"tag_id": tag_id}
+                ],
+                "returning":["post_tag_id"]
+            }
+        }
+        resp2 = requests.request("POST", data_url, data=json.dumps(requestPayload2), headers=data_headers)
+        response = decode_json(resp2)['returning'][0]['post_tag_id']
+        print(response)
+        post_tag_ids.append(response)
+    if(len(post_tag_ids) > 0):
+        return True
+    else:
+        return False
+
+def decode_json(response):
+    response = response.content
+    json_response = json.loads(response.decode('utf-8'))
+    return json_response
